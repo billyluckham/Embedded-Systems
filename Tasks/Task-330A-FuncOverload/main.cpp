dc867b5248034337bdf04635968c09764b703ef4
billyluckham <billy.luckham@students.plymouth.ac.uk>
2023-10-31 19:37:58 +0000

    task 330A

--- Tasks/Task-330A-FuncOverload/main.cpp
+++ Tasks/Task-330A-FuncOverload/main.cpp
@@ -39,6 +39,10 @@ public:
         _real = stod(strVal);
     }
 
+    void setValue(DoubleNumber n) {         // New function taking DoubleNumber as a parameter
+        _real = n.getValue();
+    }
+
     double getValue() {
         return _real;
     }
@@ -55,12 +59,15 @@ int main()
     DoubleNumber n0(1.0);
     DoubleNumber n1;
     DoubleNumber n2;
+    DoubleNumber n3;
 
     //setValue function overloading  
     n1.setValue(10);
     n2.setValue("-3.0");
+    DoubleNumber n4(5.5);
+    n3.setValue(n4);
 
-    cout << n0.getValue() + n1.getValue() + n2.getValue() << endl;
+    cout << n0.getValue() + n1.getValue() + n2.getValue() +n3.getValue() << endl;
 
     while (true) {
 
